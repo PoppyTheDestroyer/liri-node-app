f2111ebe3c297f40085113ad04d82346eaea9d88
Jason Saenz <jasonsaenz@Jasons-MacBook-Pro.local>
2017-12-05 15:24:02 -0600

    just getting started

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'zemFGjFygACX6sIQiPF8vwpOY',
+  consumer_secret: 'UsdKkfviQLubcaG3V5u3YRZk1lZEMNHmKLO6CGgXP0BlgVrFPs',
+  access_token_key: '935990768652705793-3uVuVEcdSfRFgu08yVzXphJQlrIryaf',
+  access_token_secret: '6Nxno0e01gC7BUWqB5A5R9tdBn114MmZV1jtB8rUBDMAn',
+}
+
+module.exports = twitterKeys;
