16ad09afadf7feb84c2dbf99468b9189f3dd0515
Jason Saenz <jasonsaenz@Jasons-MacBook-Pro.local>
2017-12-05 15:46:21 -0600

    keys

--- keys.js
+++ keys.js
@@ -1,10 +1,20 @@
-console.log('this is loaded');
+console.log("this is loaded");
 
 var twitterKeys = {
-  consumer_key: 'zemFGjFygACX6sIQiPF8vwpOY',
-  consumer_secret: 'UsdKkfviQLubcaG3V5u3YRZk1lZEMNHmKLO6CGgXP0BlgVrFPs',
-  access_token_key: '935990768652705793-3uVuVEcdSfRFgu08yVzXphJQlrIryaf',
-  access_token_secret: '6Nxno0e01gC7BUWqB5A5R9tdBn114MmZV1jtB8rUBDMAn',
+  consumer_key: "zemFGjFygACX6sIQiPF8vwpOY",
+  consumer_secret: "UsdKkfviQLubcaG3V5u3YRZk1lZEMNHmKLO6CGgXP0BlgVrFPs",
+  access_token_key: "935990768652705793-3uVuVEcdSfRFgu08yVzXphJQlrIryaf",
+  access_token_secret: "6Nxno0e01gC7BUWqB5A5R9tdBn114MmZV1jtB8rUBDMAn",
 }
 
+var spotifyKeys = {
+  consumer_key: "d94f04ca745c4bebb6a44a0efdb9f412",
+  consumer_secret: "38f4f039ff754564beebb31339d7b1c9"
+}
+
+var movieKeys = {
+  consumer_key: "40e9cece"
+}
 module.exports = twitterKeys;
+module.exports = spotifyKeys;
+module.exports = movieKeys;
