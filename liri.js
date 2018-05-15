55f9560deadb9748e718f885af17a5f998baf5b4
Jason Saenz <jsigns@gmail.com>
2018-05-15 17:30:42 -0500

    almost done

--- liri.js
+++ liri.js
@@ -1,35 +1,42 @@
 var webKeys = require("./keys.js");
 var twitter = require("twitter");
 var spotify = require("node-spotify-api");
-var request = require("request");
 var fs = require("fs");
 var inquirer = require("inquirer");
+var request = require("request");
 
-var userRequest = process.argv[2];
-var secondRequest = process.argv[3];
-for (i = 4; i < process.argv.length; i += 1) {
-  secondRequest += "+" + process.argv[i];
-}
 function userCommand() {
-    inquirer.prompt([
-        {
-            type: "list",
-            message: "What would you like to do?",
-            choices: ["View Jason's Tweets (they're not good)", "Look up info about a song on Spotify", "Look up info about a movie on IMDB (through OMDB)"],
-            name: "begin"
-        }
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View Jason's Tweets (they're not good)",
+          "Look up info about a song on Spotify",
+          "Look up info about a movie on IMDB (through OMDB)",
+          "Exit"
+        ],
+        name: "begin"
+      }
     ])
 
-    .then(function (inquirerResponse) {
-        if (inquirerResponse.begin === "View Jason's Tweets (they're not good)") {
-            getTweets();
-        }
-        if (inquirerResponse.begin === "Look up info about a song on Spotify") {
-            getSpotify();
-        }
-        if (inquirerResponse.begin === "Look up info about a movie on IMDB (through OMDB") {
-           getMovie();
-        }
+    .then(function(inquirerResponse) {
+      if (inquirerResponse.begin === "View Jason's Tweets (they're not good)") {
+        getTweets();
+      }
+      if (inquirerResponse.begin === "Look up info about a song on Spotify") {
+        getSpotify();
+      }
+      if (
+        inquirerResponse.begin ===
+        "Look up info about a movie on IMDB (through OMDB)"
+      ) {
+        getMovie();
+      }
+      if (inquirerResponse.begin === "Exit") {
+        "quit";
+      }
     });
 }
 
@@ -63,66 +70,90 @@ function getTweets() {
         console.log("------------------");
       }
     }
+    userCommand();
   });
 }
 
-function getSpotify(song) {
-  if (song === undefined) {
-    song = "Life During Wartime";
-  }
-  var client = new spotify({
-    id: webKeys.spotifyKeys.consumer_key,
-    secret: webKeys.spotifyKeys.consumer_secret
-  });
-
-  client.search({ type: "track", query: song }, function(error, data) {
-    if (!error) {
-      for (var i = 0; i < 5; i += 1) {
-        var songData = data.tracks.items[i];
-        //console.log (songData.album[0]);
-        console.log(
-          `\nArtist: ${songData.artists[0].name}\nSong: ${
-            songData.name
-          }\nPreview URL: ${songData.preview_url}\nAlbum: ${
-            songData.album.name
-          }\n`
-        );
+function getSpotify() {
+  inquirer
+    .prompt([
+      {
+        name: "song",
+        type: "input",
+        message: "What song would you like to look up?"
       }
-    } else {
-      console.log(error);
-    }
-  });
-}
+    ])
+    .then(function(inquirerResponse) {
+      if (inquirerResponse.song === "") {
+        song = "Life During Wartime";
+      } else {
+        song = inquirerResponse.song;
+      }
+      var client = new spotify({
+        id: webKeys.spotifyKeys.consumer_key,
+        secret: webKeys.spotifyKeys.consumer_secret
+      });
 
-function getMovie(title) {
-  if (title === undefined) {
-    title = "Goon";
-  } else {
-    title = secondRequest;
-  }
-  const url = request(
-    `http://www.omdbapi.com/?t=${title}&apikey=${
-      webKeys.movieKeys.consumer_key
-    }`,
-    function(err, req, body) {
-      if (!err && req.statusCode === 200) {
-        var jsonData = JSON.parse(body);
-        if (jsonData.Response === "False") {
-          console.log(
-            "Sorry, this title cannont be found because OMDB is vastly inferior to IMDB."
-          );
+      client.search({ type: "track", query: song }, function(error, data) {
+        if (!error) {
+          for (var i = 0; i < 5; i += 1) {
+            var songData = data.tracks.items[i];
+            //console.log (songData.album[0]);
+            console.log(
+              `\nArtist: ${songData.artists[0].name}\nSong: ${
+                songData.name
+              }\nPreview URL: ${songData.preview_url}\nAlbum: ${
+                songData.album.name
+              }\n`
+            );
+          }
         } else {
-          console.log(
-            `Title: ${jsonData.Title}\nYear: ${jsonData.Year}\nIMDB Rating: ${
-              jsonData.Ratings[0].Value
-            }\nRotten Tomatoes Rating: ${jsonData.Ratings[1].Value}\nCountry: ${
-              jsonData.Country
-            }\nPlot: ${jsonData.Plot}\nStarring: ${jsonData.Actors} `
-          );
+          console.log(error);
         }
+        userCommand();
+      });
+    });
+}
+function getMovie() {
+  inquirer
+    .prompt([
+      {
+        name: "movie",
+        type: "input",
+        message: "What movie would you like to look up?"
       }
-    }
-  );
+    ])
+    .then(function(inquirerResponse) {
+      if (inquirerResponse.movie === "") {
+        title = "Goon";
+      } else {
+        title = inquirerResponse.movie;
+      }
+     request(`http://www.omdbapi.com/?t=${title}&apikey=${webKeys.movieKeys.consumer_key}`, (err, req, body) => {
+          if (!err && req.statusCode === 200) {
+            var jsonData = JSON.parse(body);
+            if (jsonData.Response === "False") {
+              console.log(
+                "Sorry, this title cannont be found because OMDB is vastly inferior to IMDB."
+              );
+            } else {
+              console.log(
+                `Title: ${jsonData.Title}\nYear: ${
+                  jsonData.Year
+                }\nIMDB Rating: ${
+                  jsonData.Ratings[0].Value
+                }\nRotten Tomatoes Rating: ${
+                  jsonData.Ratings[1].Value
+                }\nCountry: ${jsonData.Country}\nPlot: ${
+                  jsonData.Plot
+                }\nStarring: ${jsonData.Actors} `
+              );
+            }
+            userCommand();
+          }
+        }
+      );
+    });
 }
 
 userCommand();
